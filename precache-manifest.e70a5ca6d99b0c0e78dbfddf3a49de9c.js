76b7640e0de2e1093ac660a671ec115c84e68198
russellicious <42648750+russellicious@users.noreply.github.com>
2018-10-11 14:37:04 -0500

    Updates

---
+++ precache-manifest.e70a5ca6d99b0c0e78dbfddf3a49de9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c38304c028a186cc9ba",
+    "url": "/StarWars/static/js/runtime~main.0c38304c.js"
+  },
+  {
+    "revision": "98b9cd71e247743fbdde",
+    "url": "/StarWars/static/js/main.98b9cd71.chunk.js"
+  },
+  {
+    "revision": "955f956eb89c2ca362fe",
+    "url": "/StarWars/static/js/1.955f956e.chunk.js"
+  },
+  {
+    "revision": "98b9cd71e247743fbdde",
+    "url": "/StarWars/static/css/main.91f14e27.chunk.css"
+  },
+  {
+    "revision": "a348a91b2ae0669149c2f4713fcdaef8",
+    "url": "/StarWars/index.html"
+  }
+];
